ec184b80141eecca23f58f3ea704a1bf408ad95a
overlord <rexatuzie@gmail.com>
2023-08-27 02:34:35 +0100

    gdrthjyk

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import Blog from "./pages/Blog";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import Pricing from "./pages/Pricing";
 import Service from "./pages/Service";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Register />} />
         <Route path="/about-us" element={<About />} />
         <Route path="/services" element={<Service />} />
         <Route path="/blog" element={<Blog />} />
---
+++ src/pages/Register.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { IoLogoBuffer } from "react-icons/io";
+
+const Register = () => {
+  return (
+    <div className=" w-full text-lg font-medium flex justify-center items-center h-screen">
+      <form className=" w-[90%] lg:w-[38%] ">
+        <div className=" flex items-center text-purple-600">
+          <IoLogoBuffer size={20} />
+          <h1 className=" ml-2 font-semibold text-xs  lg:text-base">ARTECH</h1>
+        </div>
+
+        <h2 className="text-xl lg:text-3xl mt-6 font-medium text-center ">
+          Create your account
+        </h2>
+        <p className="text-sm text-gray-400 text-center">
+          Let's get started with your 30 days free trail
+        </p>
+
+        <div>
+          <label className=" text-sm" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="border p-2 rounded-md block w-full"
+            type="email"
+            name="email"
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
