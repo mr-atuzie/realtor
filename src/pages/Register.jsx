a9f97ae183067bd3a33b4ec69ff85e5081544e7f
overlord <rexatuzie@gmail.com>
2023-08-27 03:39:22 +0100

    gerthyjk

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { IoLogoBuffer } from "react-icons/io";
+import { FcGoogle } from "react-icons/fc";
+import { FaFacebook, FaTwitter } from "react-icons/fa";
 
 const Register = () => {
   return (
@@ -10,17 +12,20 @@ const Register = () => {
           <h1 className=" ml-1 font-semibold text-xs  lg:text-base">ARTECH</h1>
         </div>
 
-        <h2 className="text-xl lg:text-3xl mt-4 font-medium text-center ">
-          Create your account
-        </h2>
-        <p className="text-xs text-gray-500 text-center">
-          Let's get started with your 30 days free trail
-        </p>
+        <div className=" my-4">
+          <h2 className="text-xl lg:text-3xl font-medium text-center ">
+            Create your account
+          </h2>
+          <p className="text-xs text-gray-500 text-center">
+            Let's get started with your 30 days free trail
+          </p>
+        </div>
 
         <div className=" my-4">
-          <label className=" text-sm flex items-center" htmlFor="email">
-            Email <span className=" text-purple-600">*</span>
-          </label>
+          <div className=" text-sm flex items-center">
+            <label htmlFor="email">Email</label>
+            <span className=" text-purple-600">*</span>
+          </div>
           <input
             className="border p-2 rounded-full block w-full"
             type="email"
@@ -29,9 +34,10 @@ const Register = () => {
         </div>
 
         <div className=" my-4">
-          <label className=" text-sm flex items-center" htmlFor="email">
-            Password <span className=" text-purple-600">*</span>
-          </label>
+          <div className=" text-sm flex items-center">
+            <label htmlFor="password">Password</label>
+            <span className=" text-purple-600">*</span>
+          </div>
           <input
             className="border p-2 rounded-full block w-full"
             type="password"
@@ -49,23 +55,26 @@ const Register = () => {
         <h1 className=" text-center text-lg my-4 text-gray-500">or</h1>
 
         <button
-          className=" w-full text-center py-2 my-7 bg-white border text-black font-medium rounded-full"
+          className=" w-full text-center py-2 my-7 bg-white border text-black rounded-full"
           type="submit"
         >
+          <FcGoogle />
           Sign up with Google
         </button>
 
         <button
-          className=" w-full text-center py-2 my-7 bg-white border text-black font-medium rounded-full"
+          className=" w-full text-center py-2 my-2 bg-white border text-black  rounded-full"
           type="submit"
         >
+          <FaFacebook />
           Sign up with Facebook
         </button>
 
         <button
-          className=" w-full text-center py-2 my-7 bg-white border text-black font-medium rounded-full"
+          className=" w-full text-center py-2 my-2 bg-white border text-black  rounded-full"
           type="submit"
         >
+          <FaTwitter />
           Sign up with X
         </button>
       </form>
