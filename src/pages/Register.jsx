6b2c719f6d46f34c0048d086f0c418771f4625c2
overlord <rexatuzie@gmail.com>
2023-08-27 02:50:19 +0100

    ghjkl

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -7,40 +7,40 @@ const Register = () => {
       <form className=" w-[90%] lg:w-[38%] ">
         <div className=" flex items-center text-purple-600">
           <IoLogoBuffer size={20} />
-          <h1 className=" ml-2 font-semibold text-xs  lg:text-base">ARTECH</h1>
+          <h1 className=" ml-1 font-semibold text-xs  lg:text-base">ARTECH</h1>
         </div>
 
-        <h2 className="text-xl lg:text-3xl mt-6 font-medium text-center ">
+        <h2 className="text-xl lg:text-3xl mt-4 font-medium text-center ">
           Create your account
         </h2>
-        <p className="text-sm text-gray-400 text-center">
+        <p className="text-xs text-purple-300 text-center">
           Let's get started with your 30 days free trail
         </p>
 
-        <div className=" my-7">
+        <div className=" my-4">
           <label className=" text-sm" htmlFor="email">
-            Email
+            Email<span className=" text-purple-600">*</span>
           </label>
           <input
-            className="border p-2 rounded-md block w-full"
+            className="border p-2 rounded-lg block w-full"
             type="email"
             name="email"
           />
         </div>
 
-        <div className=" my-7">
+        <div className=" my-4">
           <label className=" text-sm" htmlFor="email">
-            Password
+            Password <span className=" text-purple-600">*</span>
           </label>
           <input
-            className="border p-2 rounded-md block w-full"
+            className="border p-2 rounded-lg block w-full"
             type="password"
             name="password"
           />
         </div>
 
         <button
-          className=" my-7 bg-black text-white font-semibold rounded-md"
+          className=" w-full text-center my-7 bg-black text-white font-semibold rounded-lg"
           type="submit"
         >
           Sign up
