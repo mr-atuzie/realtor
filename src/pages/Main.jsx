6840af5ca78d36378edf28eb92d294801b2f13d7
overlord <rexatuzie@gmail.com>
2023-08-30 04:15:20 +0100

    n..

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -19,7 +19,7 @@ const Main = () => {
         </div>
 
         <div className=" bg-purple-50 flex justify-center items-center h-[50px] relative w-[50px] rounded-full">
-          <IoMdNotificationsOutline size={25} />
+          <IoMdNotificationsOutline size={30} />
           <div className=" w-2 h-2 bg-red-500 rounded-full absolute  right-2"></div>
         </div>
       </div>
@@ -27,6 +27,12 @@ const Main = () => {
         <div className=" text-white text-2xl flex items-center gap-1 font-light">
           <span className=" text-sm">$</span>
           <p>2000</p>
+        </div>
+
+        <div>
+          <button className=" bg-transparent text-white capitalize items-center rounded-md py-2.5">
+            withdraw
+          </button>
         </div>
       </div>
     </div>
