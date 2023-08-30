a19895e39f1e17d528dfeea153f67ae60327263b
overlord <rexatuzie@gmail.com>
2023-08-30 03:59:45 +0100

    gfwdckl

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -6,21 +6,25 @@ const Main = () => {
   return (
     <div className=" p-5">
       <div className=" flex items-center justify-between">
-        <img
-          src={user?.photo}
-          className=" w-[50px] h-[50px] rounded-full object-cover"
-          alt=""
-        />
+        <div className=" flex items-center gap-2">
+          <img
+            src={user?.photo}
+            className=" w-[50px] h-[50px] rounded-full object-cover"
+            alt=""
+          />
+          <h2 className=" font-medium capitalize">{user?.name}</h2>
+        </div>
 
-        <div className=" bg-purple-50 h-[50px] w-[50px] rounded-full">
+        <div className=" bg-purple-50 h-[50px] relative w-[50px] rounded-full">
           <IoMdNotificationsOutline size={25} />
+          <div className=" w-5 h-5 bg-red-500 rounded-full absolute top-0 right-2"></div>
         </div>
       </div>
-      <div className=" f">
-        <div>
-          <h2 className=" font-medium capitalize">{user?.name}</h2>
+      <div className=" my-4 bg-black">
+        {/* <div>
+          
           <p className=" text-xs text-gray-500">{user?.email}</p>
-        </div>
+        </div> */}
       </div>
     </div>
   );
