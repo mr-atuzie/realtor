ec67b7e7a2cae8d179bddd3032f832487c441e8f
overlord <rexatuzie@gmail.com>
2023-09-15 09:20:29 +0100

    fdbnmh

--- src/components/Packages.jsx
+++ src/components/Packages.jsx
@@ -12,7 +12,7 @@ const Packages = () => {
         <p className="text-gray-500 text-center">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa nobis.
         </p>
-        <div className=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-10 mt-10">
+        <div className=" grid grid-cols-6 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-10 mt-10">
           <div className=" bg-gray-900 p-4 md:p-7 h-fit shadow-md rounded-md hover:scale-105">
             <div className=" flex flex-col justify-center items-center">
               <h1 className=" text-red-500 text-lg font-semibold tracking-wide uppercase">
