2b58d60d3d879e2b66f8178b32075dc605b87674
mr-atuzie <user email>
2024-01-21 05:13:02 +0100

    shipping update

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import AllProducts from "./pages/AllProducts";
 import Navbar from "./components/Navbar";
 import Order from "./pages/Order";
 import ScrollTop from "./components/ScrollTop";
-import Shipping from "./pages/Shipping";
 import { Toaster } from "react-hot-toast";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Shipping from "./pages/Shipping";
 
 function App() {
   return (
@@ -20,11 +20,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/shipping" element={<Shipping />} />
           <Route path="/products" element={<AllProducts />} />
           <Route path="/orders" element={<Order />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/shipping" element={<Shipping />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/Shipping.jsx
+++ src/pages/Shipping.jsx
@@ -55,124 +55,122 @@ const Shipping = () => {
     console.log(userData);
   };
   return (
-    <>
-      <div className=" w-full  lg:mx-auto bg-white  ">
-        <div className=" w-[90%]  mx-auto">
-          <div className=" flex justify-between items-center mt-5">
-            <h1 className=" font-semibold text-purple-600  text-lg">
-              Delivery Form
-            </h1>
-            <IoCloseOutline
-              onClick={() => navigate(-1)}
-              size={20}
-              className=" text-red-500 "
+    <div className=" w-full bg-purple-100">
+      <div className=" h-fit w-[90%] lg:w-[45%] p-5  my-32   bg-white  mx-auto">
+        <div className=" flex justify-between items-center mt-5">
+          <h1 className=" font-semibold text-purple-600  text-lg">
+            Delivery Form
+          </h1>
+          <IoCloseOutline
+            onClick={() => navigate(-1)}
+            size={20}
+            className=" text-red-500 "
+          />
+        </div>
+        <form className="w-full my-7" onSubmit={handleSubmit}>
+          {/* name */}
+          <div className="  mb-4 ">
+            <label
+              className=" text-gray-500 text-xs lg:text-sm "
+              htmlFor="shippingname"
+            >
+              Full name
+            </label>
+            <input
+              className=" rounded border border-purple-300 w-full p-2.5 lg:p-3.5 mt-1 lg:mt-3"
+              type="text"
+              name="shippingname"
+              id="shippingname"
+              value={shippingname}
+              onChange={handleInputChange}
             />
           </div>
-          <form className="w-full my-7" onSubmit={handleSubmit}>
-            {/* name */}
-            <div className="  mb-4 ">
-              <label
-                className=" text-gray-500 text-xs lg:text-sm "
-                htmlFor="shippingname"
-              >
-                Full name
-              </label>
-              <input
-                className=" rounded border border-purple-300 w-full p-2.5 lg:p-3.5 mt-1 lg:mt-3"
-                type="text"
-                name="shippingname"
-                id="shippingname"
-                value={shippingname}
-                onChange={handleInputChange}
-              />
-            </div>
-            {/* email */}
-            <div className="  mb-4 ">
-              <label
-                className=" text-gray-500  text-xs lg:text-sm"
-                htmlFor="shippingemail"
-              >
-                Email address
-              </label>
-              <input
-                className=" rounded border border-purple-300 w-full p-2.5 lg:p-3.5 mt-1 lg:mt-3"
-                type="email"
-                name="shippingemail"
-                id="shippingemail"
-                value={shippingemail}
-                onChange={handleInputChange}
-              />
-            </div>
-            {/* number */}
-            <div className=" mb-4">
-              <label
-                className=" text-gray-500  text-xs lg:text-sm"
-                htmlFor="shippingphone"
-              >
-                Phone number
-              </label>
-              <input
-                className=" rounded border border-purple-300 w-full p-2.5 lg:p-3.5 mt-1 lg:mt-3"
-                type="text"
-                name="shippingphone"
-                id="shippingphone"
-                value={shippingphone}
-                onChange={handleInputChange}
-              />
-            </div>
-            {/* state */}
-            <div className=" mb-4">
-              <label
-                className=" text-gray-500  text-xs lg:text-sm"
-                htmlFor="state"
-              >
-                State
-              </label>
-              <select
-                name="state"
-                value={state}
-                onChange={handleInputChange}
-                id="state"
-                className="  w-full  mt-1 lg:mt-3   rounded border border-purple-300 text-gray-700  p-3 "
-              >
-                <option>Select Country</option>
-                {states.map((state) => {
-                  return (
-                    <option key={state} value={state}>
-                      {state}
-                    </option>
-                  );
-                })}
-              </select>
-            </div>
-            {/* address */}
-            <div className="mb-4  ">
-              <label
-                className=" text-gray-500  text-xs lg:text-sm"
-                htmlFor="address"
-              >
-                Address
-              </label>
-              <input
-                className="rounded border border-purple-300 w-full p-2.5 lg:p-3.5 mt-1 lg:mt-3"
-                type="text"
-                name="address"
-                value={address}
-                id="address"
-                onChange={handleInputChange}
-              />
-            </div>
-
-            <button
-              className="mt-3 rounded w-full items-center gap-2 text-xs bg-purple-600  p-3 text-white font-medium lg:text-sm"
-              disabled={loading}
+          {/* email */}
+          <div className="  mb-4 ">
+            <label
+              className=" text-gray-500  text-xs lg:text-sm"
+              htmlFor="shippingemail"
             >
-              Continue
-            </button>
-          </form>
-        </div>
+              Email address
+            </label>
+            <input
+              className=" rounded border border-purple-300 w-full p-2.5 lg:p-3.5 mt-1 lg:mt-3"
+              type="email"
+              name="shippingemail"
+              id="shippingemail"
+              value={shippingemail}
+              onChange={handleInputChange}
+            />
+          </div>
+          {/* number */}
+          <div className=" mb-4">
+            <label
+              className=" text-gray-500  text-xs lg:text-sm"
+              htmlFor="shippingphone"
+            >
+              Phone number
+            </label>
+            <input
+              className=" rounded border border-purple-300 w-full p-2.5 lg:p-3.5 mt-1 lg:mt-3"
+              type="text"
+              name="shippingphone"
+              id="shippingphone"
+              value={shippingphone}
+              onChange={handleInputChange}
+            />
+          </div>
+          {/* state */}
+          <div className=" mb-4">
+            <label
+              className=" text-gray-500  text-xs lg:text-sm"
+              htmlFor="state"
+            >
+              State
+            </label>
+            <select
+              name="state"
+              value={state}
+              onChange={handleInputChange}
+              id="state"
+              className="  w-full  mt-1 lg:mt-3   rounded border border-purple-300 text-gray-700  p-3 "
+            >
+              <option>Select Country</option>
+              {states.map((state) => {
+                return (
+                  <option key={state} value={state}>
+                    {state}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+          {/* address */}
+          <div className="mb-4  ">
+            <label
+              className=" text-gray-500  text-xs lg:text-sm"
+              htmlFor="address"
+            >
+              Address
+            </label>
+            <input
+              className="rounded border border-purple-300 w-full p-3 lg:p-3.5 mt-1 lg:mt-3"
+              type="text"
+              name="address"
+              value={address}
+              id="address"
+              onChange={handleInputChange}
+            />
+          </div>
+
+          <button
+            className="mt-3 rounded w-full items-center gap-2 text-xs bg-purple-600  p-3 text-white font-medium lg:text-sm"
+            disabled={loading}
+          >
+            Continue
+          </button>
+        </form>
       </div>
-    </>
+    </div>
   );
 };
 
