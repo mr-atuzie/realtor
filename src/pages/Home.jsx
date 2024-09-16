a44c93c5d43b921f57d021fa76721ec8fd05ac9e
mr-atuzie <user email>
2024-09-16 08:50:16 +0100

    register text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 // import Hero from "../components/Hero";
 // import About from "../components/About";
 // import Service from "../components/Service";
-import banner from "../assets/luxe salon.png";
+// import banner from "../assets/luxe salon.png";
 // import banner2 from "../assets/1.png";
 // import Product from "../components/Product";
 import bgVid from "../assets/pageantry.mp4";
@@ -47,7 +47,7 @@ const Home = () => {
     timerComponents.push(
       <div
         key={interval}
-        className="flex flex-col justify-center items-center p-1 bg-gray-900/60 text-white w-16 h-16 md:w-24 md:h-24 lg:w-28 lg:h-28 rounded-lg mx-2"
+        className="flex flex-col justify-center items-center p-1.5 bg-gray-900/60 text-white w-16 h-16 md:w-24 md:h-24 lg:w-28 lg:h-28 rounded-lg mx-2"
       >
         <span className="text-2xl font-bold md:text-4xl lg:text-5xl">
           {timeLeft[interval]}
@@ -58,42 +58,42 @@ const Home = () => {
   });
 
   return (
-    <>
-      <div className="relative h-screen overflow-hidden">
-        {/* Video Background */}
-        <video
-          autoPlay
-          muted
-          loop
-          playsInline
-          className="absolute inset-0 w-full h-full object-cover"
-        >
-          <source src={bgVid} type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+    <div className="relative h-screen overflow-hidden">
+      {/* Video Background */}
+      <video
+        autoPlay
+        muted
+        loop
+        playsInline
+        className="absolute inset-0 w-full h-full object-cover"
+      >
+        <source src={bgVid} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
 
-        {/* Content on top of the video */}
-        <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white">
-          <h1 className="text-6xl font-extrabold ">MISS UNIVERSE 2024</h1>
-          <p className="text-sm lg:text-lg md:text-2xl ">
-            Vote for your favorite contestant and make them win!
-          </p>
-          <div className="flex justify-center mx-auto my-2 items-center">
-            {timerComponents.length ? timerComponents : <span>Time's up!</span>}
-          </div>
-          <a
-            href="#contestants"
-            className="bg-pink-500 hover:bg-pink-600  text-white font-semibold py-3 px-6 rounded-lg transition-all"
-          >
-            View Contestants
-          </a>
+      {/* Content on top of the video */}
+      <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white">
+        <h1 className="text-6xl font-extrabold ">MISS UNIVERSE 2024</h1>
+        <p className="text-sm lg:text-lg md:text-2xl ">
+          Don’t miss your opportunity to be part of the biggest beauty pageantry
+          contest of the year. Register now for a chance to win exciting cash
+          prizes and showcase your beauty, talent, and confidence on the grand
+          stage
+        </p>
+        <div className="flex justify-center mx-auto my-4 items-center">
+          {timerComponents.length ? timerComponents : <span>Time's up!</span>}
         </div>
-
-        {/* Overlay  */}
-        <div className="absolute inset-0 bg-black opacity-50 z-0"></div>
+        <a
+          href="#contestants"
+          className="bg-pink-500 hover:bg-pink-600  text-white font-medium py-3 px-6 rounded-lg transition-all"
+        >
+          View Contestants
+        </a>
       </div>
-      <img className=" w-full" src={banner} alt="" />
-    </>
+
+      {/* Overlay  */}
+      <div className="absolute inset-0 bg-black opacity-50 z-0"></div>
+    </div>
   );
 };
 
