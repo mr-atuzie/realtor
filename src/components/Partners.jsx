374b2d80a4d2f5c653dcdfc46451b7bc42459d10
mr-atuzie <user email>
2024-09-19 03:50:57 +0100

    partners logo

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -7,26 +7,28 @@ const Partners = () => {
       img: "https://logomak.com/blog/wp-content/uploads/2023/09/Mastercard-logo-500x281-min.png",
     },
     {
+      img: "https://logomak.com/blog/wp-content/uploads/2023/09/McDonalds-logo-500x281-min.png",
+    },
+    {
       img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqIqYQfNEDn3oCmqg9KCOhctlHVAusITKLDA&s",
+    },
+    {
+      img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUZ61YVs3aBIsvkHzSvuD7p510jcX2hlLwNw&s",
     },
     {
       img: "https://static-00.iconduck.com/assets.00/globacom-limited-icon-2048x2048-uovm3iz4.png",
     },
-    {
-      img: "https://logomak.com/blog/wp-content/uploads/2023/09/McDonalds-logo-500x281-min.png",
-    },
+
     {
       img: "https://seeklogo.com/images/M/MTN-logo-459AAF9482-seeklogo.com.png",
     },
     {
       img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEZvyZGvu4n4g8IS-ZUURV0JnT3tFCoRSrKQ&s",
     },
-    // {
-    //   img: "https://brandcom.ng/wp-content/uploads/2024/07/WhatsApp-Image-2024-07-29-at-4.52.23-PM-1536x1404.jpeg",
-    // },
-    // {
-    //   img: "https://brandcom.ng/wp-content/uploads/2024/07/WhatsApp-Image-2024-07-29-at-4.52.23-PM-1536x1404.jpeg",
-    // },
+
+    {
+      img: "https://www.nourhan.com/wp-content/uploads/2016/09/indomie-logo.png",
+    },
     {
       img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJkT3mS6rRD5tN-EXsCTCoOx85Sg3qRMC4Pg&s",
     },
@@ -34,10 +36,10 @@ const Partners = () => {
   return (
     <div className="  py-4 lg:py-8">
       <div className=" w-[90%] lg:w-[60%] mx-auto">
-        <h1 className="text-center font-extrabold">Our Partners</h1>
+        <h1 className="text-center font-extrabold mb-2">Our Partners</h1>
 
         <Marquee direction="right" speed={70} gradient={false} pauseOnHover>
-          <div className=" grid gap-4 mb-1 items-center grid-cols-5 ">
+          <div className=" flex gap-4 mb-1 i ">
             {logos.map((logo, index) => {
               return (
                 <img
