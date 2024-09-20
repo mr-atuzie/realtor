02ce776fd60fae56c18d05f7797557c4b8367da9
mr-atuzie <user email>
2024-09-20 04:43:56 +0100

    navbar logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,12 @@
 import React, { useState } from "react";
-import { Link, NavLink } from "react-router-dom";
-import { FaTimes } from "react-icons/fa";
-import { RxHamburgerMenu } from "react-icons/rx";
-// import { selectQuantity } from "../redux/features/cartSlice";
-// import { useSelector } from "react-redux";
-import logoImg from "../assets/logo.png";
+import { NavLink } from "react-router-dom";
 
-const logo = (
-  <Link to="/">
-    <img width={60} src={logoImg} alt="" />
-  </Link>
-);
+import { GiQueenCrown } from "react-icons/gi";
 
 // const activeLink = ({ isActive }) =>
 //   isActive ? "text-purple-600" : "text-black";
 
 const Navbar = () => {
-  // const cartQty = useSelector(selectQuantity);
-
-  // const cart = (
-  //   <span className="  ">
-  //     <Link className=" flex" to={"/cart"}>
-  //       <div className=" ml-1 relative flex items-center ">
-  //         <FaShoppingBag size={20} />
-  //         <p className=" bg-purple-600 text-white h-4 w-4  font-medium text-xs text-center flex justify-center items-center rounded-full absolute -top-1 left-3">
-  //           {cartQty}
-  //         </p>
-  //       </div>
-  //     </Link>
-  //   </span>
-  // );
-
   const [showMenu, setShowMenu] = useState(false);
 
   const toggleMenu = () => {
@@ -43,14 +19,25 @@ const Navbar = () => {
 
   return (
     <header className=" bg-white w-full sticky top-0  z-40  py-1.5 shadow-lg   lg:py-3">
-      <nav className=" w-[85%] mx-auto flex justify-between items-center">
-        <RxHamburgerMenu
-          className="lg:hidden cursor-pointer"
-          size={22}
-          onClick={toggleMenu}
-        />
+      <nav className=" w-[90%] mx-auto flex justify-between items-center">
+        <div className=" text-purple-500">
+          <GiQueenCrown size={30} />
+        </div>
 
-        {logo}
+        <button className="lg:hidden cursor-pointer" onClick={toggleMenu}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6"
+          >
+            <path
+              fillRule="evenodd"
+              d="M3 6.75A.75.75 0 0 1 3.75 6h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 6.75ZM3 12a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 12Zm0 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
 
         {/* <ul className="hidden  font-medium text-gray-400 lg:flex gap-8 items-center">
           <li>
@@ -92,29 +79,33 @@ const Navbar = () => {
           className=" z-40 shadow-lg  bg-purple-50 fixed lg:hidden top-0 right-0  w-full min-h-64 p-4"
         >
           <div className=" flex justify-between items-center">
-            {logo} <FaTimes onClick={hideMenu} size={20} />
+            <div className=" text-purple-500">
+              <GiQueenCrown size={30} />
+            </div>
+
+            <button onClick={hideMenu}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-6"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
           </div>
 
           <ul className=" text-sm  px-2 flex flex-col gap-4">
             <li className="">
               <NavLink to="/">Home</NavLink>
             </li>
-            <li className="">
-              <NavLink to="/products">Shop</NavLink>
-            </li>
-            <li className="">
-              <NavLink to="/cart">Cart</NavLink>
-            </li>
-            <li className="">
-              <NavLink to="/orders">Orders</NavLink>
-            </li>
-            <li className="">
+
+            <li className="  bg-purple-600 rounded p-2 text-white font-medium ">
               <NavLink to="/login">Login/Register</NavLink>
-            </li>
-            <li>
-              <button className="  bg-purple-600 rounded p-2 text-white font-medium ">
-                Book appointment
-              </button>
             </li>
           </ul>
         </div>
