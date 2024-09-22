dabb28ebbf7c6358a25ca776ef4542e43cf391a8
mr-atuzie <user email>
2024-09-22 16:37:47 +0100

    //

--- src/pages/DasboardLayout.jsx
+++ src/pages/DasboardLayout.jsx
@@ -7,14 +7,14 @@ const DasboardLayout = () => {
   return (
     <div className=" bg-orange-50 min-h-screen ">
       <div className=" bg-white p-4">
-        <div className="flex items-center justify-center space-x-2 ">
+        <div className="flex items-center  space-x-2 ">
           <div className="text-pink-500">
             <GiLaurelCrown size={20} />
           </div>
           <h4 className="text-2xl font-bold text-gray-800">The Grand Stage</h4>
         </div>
       </div>
-      <div className=" w-[95%] md:w-[90%] lg:w-[85%] flex justify-center items-center h-full  mx-auto">
+      <div className=" w-[95%] md:w-[90%] lg:w-[85%] my-10 flex justify-center items-center h-full  mx-auto">
         <Outlet />
       </div>
       <MobileNav />
