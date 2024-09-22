c11838a5126586e5bfc038f5459df6f80acc246a
mr-atuzie <user email>
2024-09-22 16:34:10 +0100

    dashboard layout

--- src/pages/DasboardLayout.jsx
+++ src/pages/DasboardLayout.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import MobileNav from "../components/MobileNav";
+import { GiLaurelCrown } from "react-icons/gi";
 
 const DasboardLayout = () => {
   return (
-    <div className=" bg-orange-50">
-      <div className=" w-[95%] md:w-[90%] lg:w-[85%]  mx-auto">
+    <div className=" bg-orange-50 min-h-screen ">
+      <div className=" bg-white p-4">
+        <div className="flex items-center justify-center space-x-2 ">
+          <div className="text-pink-500">
+            <GiLaurelCrown size={20} />
+          </div>
+          <h4 className="text-2xl font-bold text-gray-800">The Grand Stage</h4>
+        </div>
+      </div>
+      <div className=" w-[95%] md:w-[90%] lg:w-[85%] flex justify-center items-center h-full  mx-auto">
         <Outlet />
       </div>
       <MobileNav />
