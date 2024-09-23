c1de339b5e2a297308789277070f36050674dc4e
mr-atuzie <user email>
2024-09-23 15:10:57 +0100

    //

--- src/pages/Vote.jsx
+++ src/pages/Vote.jsx
@@ -24,18 +24,70 @@ const Vote = () => {
       </div>
 
       <div>
-        <h1 className=" text-xl lg:text-3xl font-semibold">Rex atuzie</h1>
+        {/* <h1 className=" text-xl lg:text-3xl font-semibold">Rex atuzie</h1>
         <p className="text-gray-600 mb-1 text-sm lg:text-base">
           Contestant Code: <span className="font-mono">ASR34567</span>
-        </p>
+        </p> */}
+        {/* Contestant's Name */}
+        <div className="flex items-center text-sm space-x-2">
+          <span className="text-pink-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+              />
+            </svg>
+          </span>
+          <div>
+            <h2 className="font-semibold capitalize ">Name</h2>
+            <p className=" font-medium text-gray-600">ATUZIE REX HABINUCHI</p>
+          </div>
+        </div>
+
+        {/* Contestant's Code */}
+        <div className="flex items-center text-sm space-x-2">
+          <span className="text-pink-500">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 3.75 9.375v-4.5ZM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 0 1-1.125-1.125v-4.5ZM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 13.5 9.375v-4.5Z"
+              />
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.75 6.75h.75v.75h-.75v-.75ZM6.75 16.5h.75v.75h-.75v-.75ZM16.5 6.75h.75v.75h-.75v-.75ZM13.5 13.5h.75v.75h-.75v-.75ZM13.5 19.5h.75v.75h-.75v-.75ZM19.5 13.5h.75v.75h-.75v-.75ZM19.5 19.5h.75v.75h-.75v-.75ZM16.5 16.5h.75v.75h-.75v-.75Z"
+              />
+            </svg>
+          </span>
+          <div>
+            <h2 className="font-semibold capitalize ">Code</h2>
+            <p className="font-mono text-gray-600">ART9085431</p>
+          </div>
+        </div>
         <p className=" text-xs w-fit p-2 bg-green-50 rounded-md lg:text-sm text-green-600">
           Status: Active
         </p>
       </div>
 
       {/* Vote Information */}
-      <div className="my-6">
-        <h2 className=" text-xl lg:text-2xl font-semibold">Voting</h2>
+      <div className=" my-3 lg:my-6">
+        {/* <h2 className=" text-xl lg:text-2xl font-semibold">Voting</h2> */}
         <div className="flex space-x-5 lg:space-x-8">
           <div>
             <p className=" text-sm lg:text-base">Total Votes</p>
