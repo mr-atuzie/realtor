479c77c31b29d4dfbd5297ab4e91fa054b9e5136
mr-atuzie <user email>
2024-09-23 14:57:33 +0100

    vote page restructure

--- src/pages/Vote.jsx
+++ src/pages/Vote.jsx
@@ -6,6 +6,23 @@ const Vote = () => {
   ];
   return (
     <div>
+      <h3 className="text-xl font-semibold text-gray-800 mb-4 text-center">
+        Vote for Rex atuzie
+      </h3>
+
+      <div className="my-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {galleryImages.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt="Contestant Gallery"
+              className="rounded-lg"
+            />
+          ))}
+        </div>
+      </div>
+
       <div>
         <h1 className=" text-xl lg:text-3xl font-semibold">Rex atuzie</h1>
         <p className="text-gray-600 mb-1 text-sm lg:text-base">
@@ -29,76 +46,11 @@ const Vote = () => {
             <h3 className="text-xl font-bold">#23</h3>
           </div>
         </div>
-        <div className="mt-4">
-          {/* <p className="text-sm lg:text-base">
+        {/* <div className="mt-4">
+          <p className="text-sm lg:text-base">
             Voting Ends In: <span className="text-red-600">Countdown</span>
-          </p> */}
-          {/* Votes Start and End */}
-          <div className="flex items-center text-sm space-x-2 mb-3">
-            <span className="text-pink-500">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z"
-                />
-              </svg>
-            </span>
-            <div>
-              <h2 className="font-semibold capitalize text-gray-600">
-                Voting Starts
-              </h2>
-              <p className=" ">January 1, 2024</p>
-            </div>
-          </div>
-
-          <div className="flex items-center text-sm space-x-2">
-            <span className="text-pink-500">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
-                />
-              </svg>
-            </span>
-            <div>
-              <h2 className="font-semibold capitalize text-gray-600">
-                Voting Ends
-              </h2>
-              <p className="">January 31, 2024</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* Gallery Section */}
-      <div className="my-6">
-        <h2 className=" text-xl lg:text-2xl font-semibold mb-3">Gallery</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {galleryImages.map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt="Contestant Gallery"
-              className="rounded-lg"
-            />
-          ))}
-        </div>
+          </p>
+        </div> */}
       </div>
 
       <form>
