b456f6eb44d283a90b79a5235cad97e0e63eccf3
mr-atuzie <user email>
2024-09-23 11:11:04 +0100

    new dashboard structure

--- src/pages/DasboardLayout.jsx
+++ src/pages/DasboardLayout.jsx
@@ -6,6 +6,7 @@ import { GiLaurelCrown } from "react-icons/gi";
 const DasboardLayout = () => {
   return (
     <div className=" min-h-screen ">
+      {/* Dashboard header */}
       <div className=" bg-white   p-4">
         <div className="flex items-center  space-x-2 ">
           <div className="text-pink-500">
@@ -14,7 +15,7 @@ const DasboardLayout = () => {
           <h4 className="text-2xl font-bold text-gray-800">The Grand Stage</h4>
         </div>
       </div>
-      <div className="  w-[95%] md:w-[90%]  mx-auto lg:w-[85%]  flex justify-center items-center  ">
+      <div className=" w-[90%]  mx-auto lg:w-[85%] ">
         <Outlet />
       </div>
       <MobileNav />
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,211 +1,104 @@
 import React from "react";
-// import { AiOutlineBarChart, AiOutlineTrophy } from "react-icons/ai";
-// import { FaUser } from "react-icons/fa";
 import { GiLaurelCrown } from "react-icons/gi";
-// import {
-//   RiBarcodeLine,
-//   RiCalendarCheckLine,
-//   RiCalendarLine,
-//   RiSignalWifiLine,
-// } from "react-icons/ri";
-//
+
 const Dashboard = () => {
+  const galleryImages = [
+    "https://media.istockphoto.com/id/1331637318/photo/portrait-of-young-afro-woman-with-bright-make-up.jpg?s=612x612&w=0&k=20&c=F6PSgaagZEabgAfTvxE78YPhKtK-5a8yYoA_oSbGNMM=",
+    "https://curlambassadors.ca/wp-content/uploads/2023/10/hair-model-events.jpg",
+    "https://media.istockphoto.com/id/1182617710/photo/photo-of-cheerful-curly-girl-with-positive-emotions.jpg?s=612x612&w=0&k=20&c=HeCma1qBTyPS5NdvkEh-LD9rmxQSvDed_zOtmx7OYOg=",
+    "https://media.istockphoto.com/id/1306514019/photo/beautiful-girl-with-curly-hairstyle.jpg?s=612x612&w=0&k=20&c=4roFL5l37zg8gDsPuCTisDbIMnzvSGyKLwf8sjzH9oo=",
+  ];
   return (
-    <>
-      <div className=" w-full p-6 bg-white shadow-md rounded-lg space-y-6 mt-10 mb-24 lg:mb-0">
-        {/* Contestant Info */}
-        <div className="flex items-center justify-center space-x-2 mb-5">
-          <GiLaurelCrown size={30} className="text-pink-500" />
-          <h2 className="text-lg font-semibold capitalize">
-            Contestant Dashboard
-          </h2>
-        </div>
+    <div className=" w-full  space-y-6 mt-10 mb-24 lg:mb-0">
+      {/* Contestant Info */}
+      <div className="flex items-center justify-center space-x-2 mb-5">
+        <GiLaurelCrown size={30} className="text-pink-500" />
+        <h2 className="text-lg font-semibold capitalize">Dashboard</h2>
+      </div>
 
-        {/* Contestant's Name */}
-        <div className="flex items-center text-sm space-x-2">
-          <span className="text-pink-500">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-              />
-            </svg>
-          </span>
-          <div>
-            <h2 className="font-semibold capitalize text-gray-600">
-              Contestant's Name
-            </h2>
-            <p className=" font-medium">ATUZIE REX HABINUCHI</p>
-          </div>
-        </div>
-
-        {/* Contestant's Code */}
-        <div className="flex items-center text-sm space-x-2">
-          <span className="text-pink-500">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3.75 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 3.75 9.375v-4.5ZM3.75 14.625c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5a1.125 1.125 0 0 1-1.125-1.125v-4.5ZM13.5 4.875c0-.621.504-1.125 1.125-1.125h4.5c.621 0 1.125.504 1.125 1.125v4.5c0 .621-.504 1.125-1.125 1.125h-4.5A1.125 1.125 0 0 1 13.5 9.375v-4.5Z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 6.75h.75v.75h-.75v-.75ZM6.75 16.5h.75v.75h-.75v-.75ZM16.5 6.75h.75v.75h-.75v-.75ZM13.5 13.5h.75v.75h-.75v-.75ZM13.5 19.5h.75v.75h-.75v-.75ZM19.5 13.5h.75v.75h-.75v-.75ZM19.5 19.5h.75v.75h-.75v-.75ZM16.5 16.5h.75v.75h-.75v-.75Z"
-              />
-            </svg>
-          </span>
-          <div>
-            <h2 className="font-semibold capitalize text-gray-600">
-              Contestant's Code
-            </h2>
-            <p className="font-mono">ART9085431</p>
-          </div>
-        </div>
-
-        {/* Contestant's Status */}
-        <div className="flex items-center text-sm space-x-2">
-          <span className="text-pink-500">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9 12.75 11.25 15 15 9.75m-3-7.036A11.959 11.959 0 0 1 3.598 6 11.99 11.99 0 0 0 3 9.749c0 5.592 3.824 10.29 9 11.623 5.176-1.332 9-6.03 9-11.622 0-1.31-.21-2.571-.598-3.751h-.152c-3.196 0-6.1-1.248-8.25-3.285Z"
-              />
-            </svg>
-          </span>
-          <div>
-            <h2 className="font-semibold capitalize text-gray-600">
-              Contestant's Status
-            </h2>
-            <p className="">Active</p>
-          </div>
-        </div>
-
-        {/* Votes Count */}
-        <div className="flex items-center text-sm space-x-2">
-          <span className="text-pink-500">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z"
-              />
-            </svg>
-          </span>
-          <div>
-            <h2 className="font-semibold capitalize text-gray-600">
-              Votes Count
-            </h2>
-            <p className="font-mono">9,085,431</p>
-          </div>
-        </div>
-
-        {/* Votes Rank */}
-        <div className="flex items-center text-sm space-x-2">
-          <span className="text-pink-500">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M16.5 18.75h-9m9 0a3 3 0 0 1 3 3h-15a3 3 0 0 1 3-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 0 1-.982-3.172M9.497 14.25a7.454 7.454 0 0 0 .981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 0 0 7.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 0 0 2.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 0 1 2.916.52 6.003 6.003 0 0 1-5.395 4.972m0 0a6.726 6.726 0 0 1-2.749 1.35m0 0a6.772 6.772 0 0 1-3.044 0"
-              />
-            </svg>
-          </span>
-          <div>
-            <h2 className="font-semibold capitalize text-gray-600">
-              Votes Rank
-            </h2>
-            <p className="font-mono text-lg">1st</p>
-          </div>
-        </div>
-
-        {/* Votes Start and End */}
-        <div className="flex items-start text-sm space-x-2">
-          <span className="text-pink-500">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z"
-              />
-            </svg>
-          </span>
-          <div>
-            <h2 className="font-semibold capitalize text-gray-600">
-              Vote Start
-            </h2>
-            <p className=" ">January 1, 2024</p>
-          </div>
-        </div>
-
-        <div className="flex items-start text-sm space-x-2">
-          <span className="text-pink-500">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="size-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
-              />
-            </svg>
-          </span>
-          <div>
-            <h2 className="font-semibold capitalize text-gray-600">Vote End</h2>
-            <p className="">January 31, 2024</p>
-          </div>
+      {/* Header Section */}
+      <div className="flex items-center space-x-4">
+        <img
+          src="https://i.insider.com/5a0373d9ec1ade082e107b03?width=700"
+          alt="Contestant"
+          className=" w-20 h-20 lg:w-24 lg:h-24 rounded-full"
+        />
+        <div>
+          <h1 className=" text-xl lg:text-3xl font-semibold">Rex atuzie</h1>
+          <p className="text-gray-600 text-sm lg:text-base">
+            Contestant Code: <span className="font-mono">ASR34567</span>
+          </p>
+          <p className=" text-xs bg-green-50 rounded-md lg:text-sm text-green-600">
+            Status: Active
+          </p>
         </div>
       </div>
-    </>
+
+      {/* Vote Information */}
+      <div className="my-6">
+        <h2 className=" text-xl lg:text-2xl font-semibold">Voting</h2>
+        <div className="flex space-x-5 lg:space-x-8">
+          <div>
+            <p className=" text-sm lg:text-base">Total Votes</p>
+            <h3 className="text-xl font-bold">9,000</h3>
+          </div>
+          <div>
+            <p className=" text-sm lg:text-base">Current Rank</p>
+            <h3 className="text-xl font-bold">#23</h3>
+          </div>
+          <div>
+            <button className="bg-purple-600 text-sm lg:text-base text-white py-2 px-4 rounded">
+              Vote Now
+            </button>
+          </div>
+        </div>
+        <div className="mt-4">
+          <p className="text-sm lg:text-base">
+            Voting Ends In: <span className="text-red-600">Countdown</span>
+          </p>
+        </div>
+      </div>
+
+      {/* Contestant Bio */}
+      <div className="my-6">
+        <h2 className=" text-xl lg:text-2xl font-semibold">About Rex atuzie</h2>
+        <p className=" text-sm lg:text-base">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Debitis,
+          eaque!
+        </p>
+      </div>
+
+      {/* Gallery Section */}
+      <div className="my-6">
+        <h2 className=" text-xl lg:text-2xl font-semibold">Gallery</h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {galleryImages.map((image, index) => (
+            <img
+              key={index}
+              src={image}
+              alt="Contestant Gallery"
+              className="rounded-lg"
+            />
+          ))}
+        </div>
+      </div>
+
+      {/* Footer Section */}
+      <div className="my-6">
+        <h2 className=" text-xl lg:text-2xl font-semibold">Share</h2>
+        <div className="flex space-x-4">
+          <a href="*" className="text-blue-600">
+            Facebook
+          </a>
+          <a href="*" className="text-blue-400">
+            Twitter
+          </a>
+          <a href="*" className="text-pink-600">
+            Instagram
+          </a>
+        </div>
+      </div>
+    </div>
   );
 };
 
