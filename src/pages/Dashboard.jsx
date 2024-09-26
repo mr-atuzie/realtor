d9ab587eeaa02aae6530eade7716b1b7ceb476ce
mr-atuzie <user email>
2024-09-26 09:28:35 +0100

    dashboard icon

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -116,23 +116,23 @@ const Dashboard = () => {
 
       {/* Contestant Bio */}
       <div className="my-6">
-        <div className=" flex justify-between items-center">
+        <div className=" flex justify-between items-end">
           <h2 className=" lg:text-2xl font-semibold">About Rex atuzie</h2>
 
           <div>
-            <span>
+            <span className=" text-gray-500">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className=" w-4 h-4"
+                className="w-5 h-5"
               >
                 <path
                   strokeLinecap="round"
                   strokeLinejoin="round"
-                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
                 />
               </svg>
             </span>
@@ -146,18 +146,18 @@ const Dashboard = () => {
 
       {/* Gallery Section */}
       <div className="my-6">
-        <div className=" flex justify-between items-center">
+        <div className=" flex justify-between items-end">
           <h2 className=" lg:text-2xl font-semibold">Gallery</h2>
 
           <div>
-            <span>
+            <span className=" text-gray-500">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className=" w-4 h-4"
+                className=" w-5 h-5"
               >
                 <path
                   strokeLinecap="round"
@@ -173,7 +173,7 @@ const Dashboard = () => {
             </span>
           </div>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 mt-2 md:grid-cols-3 gap-4">
           {galleryImages.map((image, index) => (
             <img
               key={index}
--- src/pages/Vote.jsx
+++ src/pages/Vote.jsx
@@ -50,7 +50,7 @@ const Vote = () => {
           </span>
           <div>
             <h2 className="font-semibold capitalize ">Name</h2>
-            <p className=" font-medium text-gray-600">ATUZIE REX HABINUCHI</p>
+            <p className=" font-medium text-gray-600">Atuzie Rex Habinuchi</p>
           </div>
         </div>
 
