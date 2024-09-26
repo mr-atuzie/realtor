2914c6a5cba6250d94a5c99810ceb046a4a5a60c
mr-atuzie <user email>
2024-09-26 02:46:34 +0100

    dasboard edit icon

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -30,7 +30,7 @@ const Dashboard = () => {
           <p className="text-gray-600 mb-1 font-medium text-sm lg:text-base">
             Contestant code: <span className="font-mono">ASR34567</span>
           </p>
-          <p className=" text-xs w-fit p-2 bg-green-50 rounded-full lg:text-sm text-green-600">
+          <p className=" text-xs w-fit p-2 bg-green-50 rounded-lg lg:text-sm text-green-600">
             Active
           </p>
         </div>
@@ -116,7 +116,28 @@ const Dashboard = () => {
 
       {/* Contestant Bio */}
       <div className="my-6">
-        <h4 className=" lg:text-2xl font-semibold">About Rex atuzie</h4>
+        <div className=" flex justify-between items-center">
+          <h4 className=" lg:text-2xl font-semibold">About Rex atuzie</h4>
+
+          <div className=" border border-pink-500 text-pink-500 p-2 rounded-full">
+            <span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className=" w-4 h-4"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+                />
+              </svg>
+            </span>
+          </div>
+        </div>
         <p className=" text-sm lg:text-base">
           An enthusiastic and confident contestant with a passion for fashion
           and creative arts.
--- src/pages/EditProfile.jsx
+++ src/pages/EditProfile.jsx
@@ -78,7 +78,22 @@ const EditProfile = () => {
   return (
     <div className=" w-full mb-24">
       <div className="flex items-center justify-center space-x-2 mb-5">
-        {/* <GiLaurelCrown size={30} className="text-pink-500" /> */}
+        <span className=" text-pink-500">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className=" w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+            />
+          </svg>
+        </span>
         <h2 className=" font-semibold capitalize">Edit Profile</h2>
       </div>
       {/* Registration Form */}
@@ -160,7 +175,7 @@ const EditProfile = () => {
               className="block text-sm font-medium text-gray-700 mb-1"
               htmlFor="bio"
             >
-              bio
+              Bio
             </label>
 
             <textarea
